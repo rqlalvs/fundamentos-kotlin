8035e66320a40beaa6151f3c6581001c32fa072e
rqlalvs <raquelalves3753@gmail.com>
2022-01-09 15:11:46 -0300

    fundamentos - nullsafety

---
+++ src/main/kotlin/fundamentos/nullsafety.kt
@@ -0,0 +1,25 @@
+package fundamentos
+
+import java.lang.Exception
+
+fun main() {
+
+    var lista : List<Int?> = listOf(1, 2, null, 3)
+    var listaNullable : List<Int?>? = null
+
+//    var nome : String? = null
+//
+//    var tamanho: Int = nome?.length ?: 0
+//
+//    println(tamanho)
+
+//    var nome : String? = "Gustavo"
+//
+//    if(nome != null){
+//        println(nome.length)
+//    }
+//
+//    val toShort : Short = nome!!.length.toShort()
+
+
+}
